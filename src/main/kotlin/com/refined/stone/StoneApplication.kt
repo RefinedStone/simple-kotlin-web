3685f5dd511bf66b3d856017d4e47089ba183392
refinedstone <chem.en.9273@gmail.com>
2023-05-27 15:29:03 +0900

    setting: init settings #0

---
+++ src/main/kotlin/com/refined/stone/StoneApplication.kt
@@ -0,0 +1,11 @@
+package com.refined.stone
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StoneApplication
+
+fun main(args: Array<String>) {
+    runApplication<StoneApplication>(*args)
+}
